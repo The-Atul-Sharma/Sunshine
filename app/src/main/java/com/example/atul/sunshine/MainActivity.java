b5db086f2c402380797d5303baa210ced430d167
Atul Sharma <theatsharma@gmail.com>
2016-06-20 00:24:53 +0530

    Add list item forecast layout

---
+++ app/src/main/java/com/example/atul/sunshine/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.atul.sunshine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new ForecastFragment())
+                    .commit();
+        }
+    }
+}
